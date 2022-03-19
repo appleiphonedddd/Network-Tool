77fa9b44660977d72267c21c5cc1b33784ee474e
Egor Lee <89340462+appleiphonedddd@users.noreply.github.com>
2022-03-19 23:08:22 +0800

    Update App.py

--- App.py
+++ App.py
@@ -118,7 +118,7 @@ def Ping_Lan():
 
     for ping in range(1,255):
 
-        address = "192.168.1." + str(ping)
+        address = "192.167.2." + str(ping)
         res = subprocess.call(["ping", "-c", "1", address])
 
         if res == 0:
